da8a777b99eed77a9e8b744326aed0da852ad3e6
Martin Fenner <martin.fenner@datacite.org>
2021-06-20 09:11:49 +0200

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-// const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-})
-
-module.exports = withBundleAnalyzer({
-  images: {
-    domains: ['pbs.twimg.com']
-  }
-})
+module.exports = {
+  reactStrictMode: true
+}
