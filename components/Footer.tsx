05dc89219e379e0998e87967d011e14a025f33e5
Martin Fenner <martin.fenner@datacite.org>
2021-07-03 09:35:15 +0200

    Remove features column in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -78,65 +78,6 @@ export default function Footer() {
           </div>
           <div className='mt-2 md:mt-0'>
             <h3 className='text-base font-semibold text-gray-400 tracking-wider uppercase'>
-              Features
-            </h3>
-            <ul className='mt-1 list-none'>
-              <li>
-                <Link
-                  href='https://blog.front-matter.io/features#editor'
-                  passHref
-                >
-                  <a
-                    href='dummy'
-                    className='text-sm border-b-0 text-gray-500 hover:text-gray-400'
-                  >
-                    Easy-to-use powerful editor
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href='https://blog.front-matter.io/features#scientific'
-                  passHref
-                >
-                  <a
-                    href='dummy'
-                    className='text-sm border-b-0 text-gray-500 hover:text-gray-400'
-                  >
-                    Editing scientific content
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href='https://blog.front-matter.io/features#publishing'
-                  passHref
-                >
-                  <a
-                    href='dummy'
-                    className='text-sm border-b-0 text-gray-500 hover:text-gray-400'
-                  >
-                    Flexible publishing options
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href='https://blog.front-matter.io/features#discovery'
-                  passHref
-                >
-                  <a
-                    href='dummy'
-                    className='text-sm border-b-0 text-gray-500 hover:text-gray-400'
-                  >
-                    Discovery, discussion and reuse
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className='mt-2 md:mt-0'>
-            <h3 className='text-base font-semibold text-gray-400 tracking-wider uppercase'>
               Services
             </h3>
             <ul className='mt-1 list-none'>
