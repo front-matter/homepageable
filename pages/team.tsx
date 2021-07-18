3372856d0fc7a8f42872c3039b87e288ea3ca304
Martin Fenner <martin.fenner@datacite.org>
2021-07-18 18:24:15 +0200

    update footer

---
+++ pages/team.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Team from '../components/Team'
+import Footer from '../components/Footer'
+
+const TeamPage = () => {
+  return (
+    <>
+      <Team />
+      <Footer />
+    </>
+  )
+}
+
+export default TeamPage
