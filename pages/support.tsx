81da039aff19e4ee6477fa79938a30eabf04d72c
Martin Fenner <martin@front-matter.io>
2021-08-09 17:00:25 +0200

    add support page

---
+++ pages/support.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Support from '../components/Support'
+import Footer from '../components/Footer'
+
+const SupportPage = () => {
+  return (
+    <>
+      <Support />
+      <Footer />
+    </>
+  )
+}
+
+export default SupportPage
