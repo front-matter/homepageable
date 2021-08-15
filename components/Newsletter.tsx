bffe6cd6af118fed3b5a137f88ef2d2586581ca1
Martin Fenner <martin@front-matter.io>
2021-08-15 20:54:48 +0200

    rename email field

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -39,8 +39,7 @@ export default function Newsletter() {
               <div className='mt-1 mb-6 sm:mt-0 sm:col-span-2'>
                 <input
                   type='email'
-                  name='email'
-                  id='email'
+                  name='fields[email]'
                   placeholder='Email'
                   className='max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md'
                 />
