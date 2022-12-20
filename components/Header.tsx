6a1882f59d7527e7f72a8dcf95ca952cea71ef4b
Martin Fenner <martin@front-matter.io>
2022-12-20 19:52:36 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBoxOpen } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar(props) {
   return (
@@ -13,7 +11,7 @@ export default function Navbar(props) {
             <div className='flex mx-2 px-2 lg:px-0'>
               <div className='flex-shrink-0 flex items-center'>
                 <Link href='/' className='text-lg text-green-600 pl-6 space-x-2 border-b-0 flex items-center'>
-                  <Image src='/images/logo.svg' alt='Front Matter' width={228} height={36} />
+                  <Image src='/images/logo.svg' alt='Front Matter' width={228} height={36} layout="responsive" />
                 </Link>
               </div>
             </div>
