0a3535611f829e332ffdeec23a0e97f8cf4f9f9d
Martin Fenner <martin@front-matter.io>
2022-12-21 11:55:48 +0100

    improve logo

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin']
+})
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang='en' className={roboto.className}>
+      <body>{children}</body>
+    </html>
+  )
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,6 @@ export default function Navbar(props) {
                 <Image
                   src={Logo}
                   alt="Logo"
-                 placeholder="blur"
                 />
                 </Link>
               </div>
